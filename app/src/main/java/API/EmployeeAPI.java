c2e905709b7fc89dd63c8f6455cb9e2978ba8e45
Sadikshya Shrestha <shresthasadikshya2@gmail.com>
2019-05-14 07:54:51 +0545

    Initial commit

---
+++ app/src/main/java/API/EmployeeAPI.java
@@ -0,0 +1,12 @@
+package API;
+
+import java.util.List;
+
+import model.Employee;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EmployeeAPI {
+    @GET("employees")
+    Call<List<Employee>> getEmployee();
+}
